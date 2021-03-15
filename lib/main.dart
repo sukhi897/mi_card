b7ba63b3ea71d9219b65477852844ae1cf6ff70d
Sukhwinder Singh <singh@sukhi.codes>
2021-03-14 19:02:20 -0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                //child: Text('Container 1'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                    //child: Text('Container 1'),
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green,
+                    //child: Text('Container 2'),
+                  ),
+                ],
+              ),
+
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                //child: Text('Container 3'),
+              ),
+
+              // Container(
+              //   width: double.infinity,
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
