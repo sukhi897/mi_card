8cc57bd0852f05ee24688463b92dccaffe114dec
Sukhwinder Singh <singh@sukhi.codes>
2021-03-16 10:50:12 -0700

    Got fonts working. Used Material widgets like Cards. Refined UI.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightGreen.shade900,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                   radius: 63.0,
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
               Text(
                 'Sukhwinder Singh',
                 style: TextStyle(
-                  fontFamily: 'Pacifico',
+                  fontFamily: 'Redressed',
                   fontSize: 40,
                   color: Colors.white,
                   //fontWeight: FontWeight.bold,
@@ -30,9 +31,64 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'SourceSansPro',
                   fontSize: 20,
-                  color: Colors.lightGreen.shade100,
+                  color: Colors.blueGrey.shade100,
                   fontWeight: FontWeight.w900,
                   letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200,
+                child: Divider(
+                  color: Colors.blueGrey.shade900,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.blueGrey,
+                      ),
+                      SizedBox(
+                        width: 12.0,
+                      ),
+                      Text(
+                        '+1 559 367 8272',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey.shade900,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.blueGrey,
+                      ),
+                      SizedBox(
+                        width: 12.0,
+                      ),
+                      Text(
+                        'singh@sukhi.codes',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blueGrey.shade900,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
